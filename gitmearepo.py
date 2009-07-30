9c6cbb443d76718c706816e709cc00573e96133c
Bill Katz <billkatz@gmail.com>
2009-07-30 01:55:21 -0700

    Modified how a primary language is determined for a repo.

--- gitmearepo.py
+++ gitmearepo.py
@@ -19,8 +19,7 @@ UNKNOWN_LANGUAGE = 'Unknown'
 DEFAULT_NUM_LINES = 1000
 
 user_watching = {}      # Key: user id => Value: list of repo ids
-user_langs = {}         # Key: user id => Value: dict of {language: # lines}
-repo_data = {}          # Key: repo id => Value: (username, repo name, date, fork repo id)
+repo_data = {}          # Key: repo id => Value: (username, repo name, primary language, fork repo id)
 repo_headcount = {}     # Key: repo id => Value: # of watchers
 repo_langs = {}         # Key: repo id => Value: list of (language, # lines) tuples.
 languages = []
@@ -44,53 +43,12 @@ print "Language breakdown shows %d languages: %s" % \
 
 def repo_info(repo_id):
     global repo_data
+    if not repo_id:
+        return 'None'
     repo_datum = repo_data[repo_id]
-    text = '/'.join(repo_datum[:2])
-    if len(repo_datum) > 2 and repo_datum[2]:
-        text += ' (forked from %s)' % (repo_info(repo_datum[2]))
+    text = "%s (primary language: %s, fork from %s)" % \
+           ('/'.join(repo_datum[:2]), str(repo_datum[2]), repo_info(repo_datum[3]))
     return text
-
-REQ_LANG_AMOUNT = 0.75
-def get_repo_language(repo_id):
-    global repo_langs
-    total_lines = 0
-    most_lines = 0
-    main_language = None
-    for lang_data in repo_langs[repo_id]:
-        lang, lines = lang_data
-        total_lines += lines
-        if lines > most_lines:
-            most_lines = lines
-            main_language = lang
-    if most_lines > REQ_LANG_AMOUNT * total_lines:
-        return main_language
-    else:
-        return None
-        
-def add_to_user_prefs(user_id, repo_id):
-    global repo_languages, user_langs
-    if user_id not in user_langs:
-        user_langs[user_id] = {}
-    if repo_id in repo_langs:
-        lang_list = repo_langs[repo_id]
-    else:
-        lang_list = [(UNKNOWN_LANGUAGE, 0)]
-    for lang_data in lang_list:
-        lang, lines = lang_data
-        user_langs[user_id][lang] = (user_langs[user_id].get(lang, 0) + lines)
-
-data_file = open('data.txt', 'r')
-try:
-    for line in data_file:
-        user_id, repo_id = line.strip().split(':')
-        add_to_user_prefs(user_id, repo_id)
-        if user_id in user_watching:
-            user_watching[user_id].append(repo_id)
-        else:
-            user_watching[user_id] = [repo_id]
-        repo_headcount[repo_id] = (repo_headcount.get(repo_id, 0) + 1)
-finally:
-    data_file.close()
 
 repo_file = open('repos.txt', 'r')
 try:
@@ -98,12 +56,30 @@ try:
         repo_id, repo_text = line.strip().split(':')
         parts = repo_text.split(',')
         user_name, repo_name = parts[0].split('/')
-        forked_repo_id = parts[-1] if len(parts) > 2 else None
-        repo_data[repo_id] = (user_name, repo_name, forked_repo_id)
-        if repo_id not in repo_langs:
-            repo_langs[repo_id] = [(UNKNOWN_LANGUAGE, DEFAULT_NUM_LINES)]
+        forked_id = parts[-1] if len(parts) > 2 else None
+        main_lang = None
+        if repo_id in repo_langs:
+            lang_list = repo_langs[repo_id]
+            lang_list.sort(key=lambda x: x[1], reverse=True)
+            if len(lang_list) < 2 or lang_list[0][1] > 2 * lang_list[1][1]:
+                main_lang = lang_list[0][0]
+            else:
+                main_lang = None
+        repo_data[repo_id] = (user_name, repo_name, main_lang, forked_id)
 finally:
     repo_file.close()
+
+data_file = open('data.txt', 'r')
+try:
+    for line in data_file:
+        user_id, repo_id = line.strip().split(':')
+        if user_id in user_watching:
+            user_watching[user_id].append(repo_id)
+        else:
+            user_watching[user_id] = [repo_id]
+        repo_headcount[repo_id] = (repo_headcount.get(repo_id, 0) + 1)
+finally:
+    data_file.close()
 
 # Sort the repos in descending order of number of watchers
 repo_list = repo_headcount.items()
@@ -119,7 +95,7 @@ N = 10
 language_top_repos = {}
 for watch_data in repo_list:
     repo_id, watchers = watch_data
-    primary_language = get_repo_language(repo_id)
+    primary_language = repo_data[repo_id][2]
     if primary_language:
         if primary_language not in language_top_repos:
             language_top_repos[primary_language] = [repo_id]
@@ -135,6 +111,7 @@ for lang in language_top_repos:
 # Assume there's some kind of Long Tail for repository popularity.
 # For each user, suggest the most popular repos for that user's
 # favorite languages.
+default_best_repos = [repo_id for repo_id, watchers in repo_list[:N]]
 favorite_languages = {}
 non_watcher = 0
 result_file = open('results.txt', 'w')
@@ -142,29 +119,37 @@ test_file = open('test.txt', 'r')
 try:
     for line in test_file:
         user_id = line.strip()
-        if user_id not in user_langs:
+        if user_id not in user_watching:
             non_watcher += 1
-            # Since we have no clue about language or tastes, go with popular
-            best_repos = [repo_id for repo_id, watchers in repo_list[:N]]
+            best_repos = default_best_repos
         else:
-            lang_list = user_langs[user_id].items()
-            lang_list.sort(key=lambda x: x[1], reverse=True)
+            languages = {}
+            for repo_id in user_watching[user_id]:
+                lang = repo_data[repo_id][2]
+                if lang:
+                    languages[lang] = languages.get(lang, 0) + 1
+            if not languages:
+                best_repos = default_best_repos
+            else:
+                lang_list = languages.items()
+                lang_list.sort(key=lambda x: x[1], reverse=True)
 
-            lang, lines = lang_list[0]
-            favorite_languages[lang] = (favorite_languages.get(lang, 0) + 1)
-
-            # Go through user's language in descending popularity
-            # and find top repos until we get at least N.
-            best_repos = []
-            num = 0
-            for lang_data in lang_list:
-                lang, lines = lang_data
-                for repo_id in language_top_repos[lang]:
-                    if repo_id not in best_repos:
-                        best_repos.append(repo_id)
-                        if len(best_repos) >= N:
-                            break
-            # TODO -- Do something additional processing
+                lang, times = lang_list[0]
+                if times == 0:
+                    best_repos = default_best_repos
+                else:
+                    favorite_languages[lang] = favorite_languages.get(lang, 0) + 1
+                    # Go through user's language in descending popularity
+                    # and find top repos until we get at least N.
+                    best_repos = []
+                    num = 0
+                    for lang_data in lang_list:
+                        lang, times = lang_data
+                        for repo_id in language_top_repos[lang]:
+                            if repo_id not in best_repos:
+                                best_repos.append(repo_id)
+                                if len(best_repos) >= N:
+                                    break
         result_file.write("%s:%s\n" % (user_id, ','.join(best_repos[:N])))
 finally:
     result_file.close()
